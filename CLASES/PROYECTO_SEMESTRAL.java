32d6d7608d560fd9441a860bd9d380ffe681da23
Fernando Cutire <50190625+FernandoC1217@users.noreply.github.com>
2019-11-27 14:41:03 -0500

    añadiendo la nueva clase del archivo anterior
    añadiendo la nueva clase del archivo anterior

--- CLASES/PROYECTO_SEMESTRAL.java
+++ CLASES/PROYECTO_SEMESTRAL.java
@@ -1,9 +1,29 @@
-
+import java.util.Scanner;
 
 public class PROYECTO_SEMESTRAL {
 
+
+  // METODOS DEL MENU PRINCIPAL (000)
+  public void ImprimirPantallaPrincipal() {
+    System.out.println("\n---------------------------------------");
+    System.out.println("           JUEGO DE TRIVIA");
+    System.out.println("1 - Menu");
+    System.out.println("2 - Atender a Cliente");
+    System.out.println("3 - Reportes");
+    System.out.println("4 - Salir del día");
+    System.out.println("---------------------------------------");
+  }
+
+
+
   public static void main(String[] args) {
-    System.out.println("Empezamos el proyecto semestral");
+
+    Scanner sc = new Scanner(System.in);
+    PROYECTO_SEMESTRAL proyecto_semestral = new PROYECTO_SEMESTRAL();
+
+
+    proyecto_semestral.ImprimirPantallaPrincipal();
+
   }
 
 }
